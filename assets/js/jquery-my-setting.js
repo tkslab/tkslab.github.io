5f9dce3189a9034997d03ddfe8b95ab86513b27c
tkslab <71773745+tkslab@users.noreply.github.com>
2021-02-21 23:30:32 +0900

    Update jquery-my-setting.js

--- assets/js/jquery-my-setting.js
+++ assets/js/jquery-my-setting.js
@@ -133,3 +133,29 @@ $(function() {
 		$(elem).html(tmpHTML);
 	});
 });
+
+
+$(function () {
+    $('a[href^="#"]').click(function () {
+        var speed = 400;
+        var href = $(this).attr('href');
+        var target = $(href == '#' || href == '' ? 'html' : href);
+        var position = target.offset().top;
+        $('body,html').animate({
+            scrollTop: position
+        }, speed, 'swing');
+        return false;
+    });
+});
+
+$('input[name="accordion-toc"]').change(function () {
+    if ($(this).prop('checked')) {
+        $('[name="accordion-toclist"]').prop('checked', false);
+    }
+});
+$('input[name="accordion-toclist"]').change(function () {
+    if ($(this).prop('checked')) {
+        $('[name="accordion-toc"]').prop('checked', false);
+    }
+});
+
