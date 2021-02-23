25e27aa3834e32eb6613f37af58cf7e900b224c8
tkslab <71773745+tkslab@users.noreply.github.com>
2021-02-24 01:05:25 +0900

    Update jquery-my-breadcrumb-list.js

--- assets/js/jquery-my-breadcrumb-list.js
+++ assets/js/jquery-my-breadcrumb-list.js
@@ -1,45 +1,48 @@
-$(function () {
-    
-var path = location.pathname;
+$(function() {
 
-var dir_list = path.replace("law/jp", "law@jp").split("/");
+    var path = location.pathname;
 
-var url_list = dir_list.map((value, index) => {
-	return dir_list.slice(0, index + 1)
-});
+    var dir_list = path.replace("law/jp", "law@jp").split("/");
 
-
-var text_pre = ["law@jp", "courses", "tools", "administrative-law", "cases"];
-var text_post = ["法学資料（日本）", "講義・演習・課外講座", "ツール", "行政法", "判例一覧"];
+    var url_list = dir_list.map((value, index) => {
+        return dir_list.slice(0, index + 1)
+    });
 
 
-dir_list[0] = "Home";
-dir_list = dir_list.map((value) => {
-	text_pre.forEach((elem, index) => {
-		value = value.replace(elem, text_post[index]);
-	});
-	return value
-});
-
-// text と url をまとめたリスト生成
-var text_and_url_list = url_list.map((value, index) => {
-	return [dir_list[index], value.join("/").replace("law@jp", "law/jp")]
-});
-
-// text が空白のときには対応する url を除去
-text_and_url_list = text_and_url_list.filter((value)  => {
-  return value[0] != "";
-});
-
-// 末尾の項目を除去
-text_and_url_list.pop();
-
-// html として出力するよう整形
-text_and_url_list = text_and_url_list.map((value) => {
-	return `<li><a href='${value[1]}/'>${value[0]}</a></li>`
-});
+    var replace_pattern = [["law@jp", "法学資料（日本）"]
+    , ["courses", "講義・演習・課外講座"]
+    , ["tools", "ツール"]
+    , ["administrative-law", "行政法"]
+    , ["cases", "判例一覧"]];
 
 
-$("h1#page-title").before(`<nav><ul class="breadcrumbs">${text_and_url_list.join("\n")}</ul></nav>`);
+    dir_list[0] = "Home";
+    dir_list = dir_list.map((value) => {
+        replace_pattern.forEach((elem) => {
+            value = value.replace(elem[0], elem[1]);
+        });
+        return value
+    });
+
+    // text と url をまとめたリスト生成
+    var text_and_url_list = url_list.map((value, index) => {
+        return [dir_list[index], value.join("/").replace("law@jp", "law/jp")]
+    });
+
+    // text が空白のときには対応する url を除去
+    text_and_url_list = text_and_url_list.filter((value) => {
+        return value[0] != "";
+    });
+
+    // 末尾の項目を除去
+    text_and_url_list.pop();
+
+    // html として出力するよう整形
+    text_and_url_list = text_and_url_list.map((value) => {
+        return `<li><a href='${value[1]}/'>${value[0]}</a></li>`
+    });
+
+
+    $("div.sidebar").before(`<nav><ul class="breadcrumbs">${text_and_url_list.join("\n")}</ul></nav>`);
 
 });
