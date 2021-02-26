12367a70c27d80c325a0a7041ab0bf300fe9070d
tkslab <71773745+tkslab@users.noreply.github.com>
2021-02-26 18:36:58 +0900

    Update jquery-my-paragraph-numbers-and-indent.js

--- assets/js/jquery-my-paragraph-numbers-and-indent.js
+++ assets/js/jquery-my-paragraph-numbers-and-indent.js
@@ -1,30 +1,31 @@
 $(function() {
-  // cases ディレクトリ以下のファイルに対しては
-  // 段落番号を表示する
-  // それ以外のディレクトリ以下のファイルに対しては
-  // 冒頭に一文字分の空白を挿入
-	
-  var result = location.pathname.split("/").filter(function(elem) {
-    return (elem == "cases");
-	});
+    // cases ディレクトリ以下のファイルに対しては
+    // 段落番号を表示する
+    // それ以外のディレクトリ以下のファイルに対しては
+    // 冒頭に一文字分の空白を挿入
 
-	if (result.length == 0) {
-		$('p').each(function(_, elem) {
-			$(elem).prepend(`　`);
-		});
-			
-		return False
-	}
+    var under_cases_directory = location.pathname.split("/").some(function(elem) {
+        return (elem == "cases");
+    });
 
-	let para_index = 0;
-	$('p').each(function(_, elem) {
-		if ($(elem).closest("li").length > 0) {
-			return true
-		}
 
-		para_index += 1;
-		$(elem).prepend(
-			`[<a href="#id_${para_index}">${para_index}</a>]<a id="id_${para_index}"></a>　`
-		);
-	});
+    if (under_cases_directory) {
+        $('p').each(function(_, elem) {
+            $(elem).prepend(`　`);
+        });
+
+        return False
+    }
+
+    let para_index = 0;
+    $('p').each(function(_, elem) {
+        if ($(elem).closest("li").length > 0) {
+            return true
+        }
+
+        para_index += 1;
+        $(elem).prepend(
+            `[<a href="#id_${para_index}">${para_index}</a>]<a id="id_${para_index}"></a>　`
+        );
+    });
 });
