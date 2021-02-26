742df239a84653a41d03cc626a432215e865eebd
tkslab <71773745+tkslab@users.noreply.github.com>
2021-02-26 18:29:02 +0900

    Update jquery-my-paragraph-numbers.js

--- assets/js/jquery-my-paragraph-numbers.js
+++ assets/js/jquery-my-paragraph-numbers.js
@@ -1,11 +1,18 @@
 $(function() {
-  // cases ディレクトリ以下のファイルに対してのみ
+  // cases ディレクトリ以下のファイルに対しては
   // 段落番号を表示する
+  // それ以外のディレクトリ以下のファイルに対しては
+  // 冒頭に一文字分の空白を挿入
+	
   var result = location.pathname.split("/").filter(function(elem) {
     return (elem == "cases");
 	});
 
 	if (result.length == 0) {
+		$('p').each(function(_, elem) {
+			$(elem).prepend(`　`);
+		});
+			
 		return False
 	}
 
