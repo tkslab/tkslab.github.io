a8bb0d06f991967c2d0d97221797e064f5ed4af7
tkslab <71773745+tkslab@users.noreply.github.com>
2021-03-28 23:55:57 +0900

    Update grouping-3.cr

--- tools/grouping-3.cr
+++ tools/grouping-3.cr
@@ -12,18 +12,15 @@ class Array
   def my_fill_with_string
     number = @number_of_elements_in_one_group
     if self.size.modulo(number) > 0
-      (number - self.size.modulo(number)).times { |i|
-        self.push("dummy#{i + 1}")
+      (number - self.size.modulo(number)).times { |_|
+        self.push("dummy")
       }
     end
   end
 
   # ダミー文字列を除去
   def remove_filler
-    result = self.map { |group|
-      group = group.join(" ").gsub(/dummy\d/, "").split(" ")
-      group.select { |elem| elem != "" }
-    }
+    result = self.map { |group| group.reject("dummy") }
     self.replace(result)
   end
 
@@ -39,33 +36,33 @@ end
 
 ARGV.each do |max_number|
   # max_number = 7
+  max_number = max_number.to_i
 
   # 1グループあたりの人数
   number_of_elements_in_one_group = 3
 
-  source = (1..max_number.to_i).to_a.map { |item| sprintf("%02d", item) }
+  source = ("A".."Z").to_a.delete_at(0, max_number)
 
   # グループ分けしやすいようダミー文字列で埋めておく
   source.set_number_of_elements_in_one_group(number_of_elements_in_one_group)
   source.my_fill_with_string
 
   # 人の組み合わせを求める
-  people_pattern = source.combinations(number_of_elements_in_one_group)
+  pattern_of_elements = source.combinations(number_of_elements_in_one_group)
 
-  people_pattern.remove_filler
-  people_pattern.uniq!
-  people_pattern.remove_group_with_only_one_element
+  pattern_of_elements.remove_filler
+  pattern_of_elements.uniq!
+  pattern_of_elements.remove_group_with_only_one_element
 
   # 1回目、2回目……n回目のグループの組み合わせを求める
-  group_pattern = people_pattern.combinations(source.number_of_groups)
+  group_pattern = pattern_of_elements.combinations(source.number_of_groups)
 
-  # 1人が同時に複数のグループに属する場合を除外し、そして
-  # 全員が現れない場合も除外
+  # 全員が出現すると同時に
+  # 1人が同時に複数のグループに属していない場合を抽出
   group_pattern.select! { |item|
-    item2 = item.flatten
-    (item2.size - item2.uniq.size) == 0 && max_number.to_i == item2.size
+    size = item.flatten.size
+    size == max_number && (size - item.flatten.uniq.size) == 0
   }
-
 
   # 一度出現したグループが見つかったらスキップ
   uniq_group = Array(Array(String)).new
