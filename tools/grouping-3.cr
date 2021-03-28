84e74a1d156db89dbe7ffe6f23aae8cf88401eea
tkslab <71773745+tkslab@users.noreply.github.com>
2021-03-29 00:48:20 +0900

    Update grouping-3.cr

--- tools/grouping-3.cr
+++ tools/grouping-3.cr
@@ -18,7 +18,6 @@ class Array
     end
   end
 
-  # ダミー文字列を除去
   def remove_filler
     result = self.map { |group| group.reject("dummy") }
     self.replace(result)
@@ -35,53 +34,53 @@ class Array
 end
 
 ARGV.each do |max_number|
-  # max_number = 7
+  # max_number = 5
   max_number = max_number.to_i
-
-  # 1グループあたりの人数
-  number_of_elements_in_one_group = 3
 
   source = ("A".."Z").to_a.delete_at(0, max_number)
 
-  # グループ分けしやすいようダミー文字列で埋めておく
+  # 1グループあたりの要素
+  number_of_elements_in_one_group = 3
+
+  # グループ分けしやすいようダミー文字列を要素として挿入する
   source.set_number_of_elements_in_one_group(number_of_elements_in_one_group)
   source.my_fill_with_string
 
-  # 人の組み合わせを求める
+  # 要素の組み合わせを求める
   pattern_of_elements = source.combinations(number_of_elements_in_one_group)
 
   pattern_of_elements.remove_filler
   pattern_of_elements.uniq!
   pattern_of_elements.remove_group_with_only_one_element
 
-  # 1回目、2回目……n回目のグループの組み合わせを求める
-  group_pattern = pattern_of_elements.combinations(source.number_of_groups)
+  # グループの組み合わせをすべて求める
+  pattern_of_groups = pattern_of_elements.combinations(source.number_of_groups)
 
-  # 全員が出現すると同時に
-  # 1人が同時に複数のグループに属していない場合を抽出
-  group_pattern.select! { |item|
+  pattern_of_groups_length_valid = [] of Array(Array(String))
+
+  # 全要素が出現していて、かつ
+  # ある1つの要素が複数のグループに出現していない場合を抽出する
+  pattern_of_groups.each { |item|
     size = item.flatten.size
-    size == max_number && (size - item.flatten.uniq.size) == 0
+    if (size == max_number && (size - item.flatten.uniq.size) == 0)
+      pattern_of_groups_length_valid.push(item)
+    end
   }
+  
+  # 既に出現したグループが一部に見つかったら除去する
+  uniq_groups = Array(Array(String)).new
 
-  # 一度出現したグループが見つかったらスキップ
-  uniq_group = Array(Array(String)).new
-
-  group_pattern.select! { |groups|
-    result = groups.each.all? { |group|
-      !uniq_group.find { |uniq_pattern| group == uniq_pattern }
+  pattern_of_groups_length_valid.select! {|groups|
+    result = groups.each.all? {|group|
+      !uniq_groups.find {|uniq_pattern| group == uniq_pattern }
     }
 
-    if result
-      groups.each { |group|
-        uniq_group.push(group)
-      }
-    end
+    groups.each {|group| uniq_groups.push(group) } if result
 
     result
   }
 
-  group_pattern.each.with_index(1) do |element, index|
+  pattern_of_groups_length_valid.each.with_index(1) do |element, index|
     puts "#{sprintf("%02d", index)}：#{element.join(", ")}"
   end
 end
